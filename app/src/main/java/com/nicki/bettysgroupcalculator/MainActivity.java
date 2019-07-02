4c5f8be96d31533d261265c1f1036e05685cc163
AC2 <donoloo4545@gmail.com>
2019-07-02 16:24:28 +0300

    the first update

--- app/src/main/java/com/nicki/bettysgroupcalculator/MainActivity.java
+++ app/src/main/java/com/nicki/bettysgroupcalculator/MainActivity.java
@@ -2,17 +2,39 @@ package com.nicki.bettysgroupcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    private TextView myNumber;
+    private Button  cancel;
+    private Button  c;
+    private  Button division;
+    private  Button multiplication;
+    private  Button  delete;
+    private  Button seven;
+    private  Button  eigt;
+    private  Button  nine;
+    private  Button addition;
+    private  Button  four;
+    private  Button five;
+    private Button  six;
+    private Button subtraction;
+    private Button  one;
+    private Button two;
+    private Button three;
+    private Button equal;
+    private Button  zero;
+    private Button point;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
 
